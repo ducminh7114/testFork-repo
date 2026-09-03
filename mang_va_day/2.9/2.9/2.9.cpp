b8c4c23617435c54e535243740bd36ed4d2d7f62
ducminh7114 <doducminh7114@gmail.com>
2026-09-03 14:49:25 +0700

    update 2.9
    update 2.9

--- mang_va_day/2.9/2.9/2.9.cpp
+++ mang_va_day/2.9/2.9/2.9.cpp
@@ -1,8 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-	int soLuong;
-	int arr;
+    int a[] = {7, 4, 8, 3, 2, 6, 9};
+    int n = 7;
 
+    int b[100];
+    int m = 0;
+
+    // Lấy các số chẵn
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            b[m] = a[i];
+            m++;
+        }
+    }
+
+    // Sắp xếp tăng dần
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (b[i] > b[j]) {
+                int temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+
+    // Đưa các số chẵn đã sắp xếp trở lại mảng a
+    m = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            a[i] = b[m];
+            m++;
+        }
+    }
+
+    // In mảng
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
 }
